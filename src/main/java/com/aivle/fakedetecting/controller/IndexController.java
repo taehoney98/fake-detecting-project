8360be0ce342e73e7d0d30774071375a2793a80b
yodong <jeongs741@naver.com>
2025-01-09 22:02:59 +0900

    Start page Index, UpLoad not FAST API

---
+++ src/main/java/com/aivle/fakedetecting/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.aivle.fakedetecting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){ return "index"; }
+
+    @GetMapping("/sign_up")
+    public String signup(){return "sign-up";}
+
+    @GetMapping("/login")
+    public String login(){return "login";}
+
+}
---
+++ src/main/java/com/aivle/fakedetecting/controller/UpLoadController.java
@@ -0,0 +1,13 @@
+package com.aivle.fakedetecting.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class UpLoadController {
+
+
+}
