6f4f0c8da0562224a440f2cf14902f8de8c6b707
jongjin <kakao1302@naver.com>
2025-02-03 17:51:32 +0900

    분석내역 Paging 적용

--- src/main/java/com/aivle/fakedetecting/repository/AnalysisRepository.java
+++ src/main/java/com/aivle/fakedetecting/repository/AnalysisRepository.java
@@ -1,6 +1,8 @@
 package com.aivle.fakedetecting.repository;
 
 import com.aivle.fakedetecting.entity.Analysis;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import java.util.List;
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
     List<Analysis> findByMember_Seq(Long seq);
 
-    List<Analysis> findByMember_SeqOrderByCreateDateDesc(Long seq);
+    Page<Analysis> findAllByMember_SeqOrderByCreateDateDesc(Long seq, Pageable pageable);
+//    List<Analysis> findByMember_SeqOrderByCreateDateDesc(Long seq);
 }
