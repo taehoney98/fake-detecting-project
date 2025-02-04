89b7bd833be4e6c0f7b0fe33e64bde2d751effb0
jongjin <kakao1302@naver.com>
2025-02-04 11:01:45 +0900

    결제정보 요구로 ResponseLogin수정, cors origins 수정

--- src/main/java/com/aivle/fakedetecting/config/SecurityConfig.java
+++ src/main/java/com/aivle/fakedetecting/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://http://3.36.77.31/:3000")
+                        .allowedOrigins("*")
+//                        .allowedOrigins("http://3.36.77.31:3000")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .exposedHeaders("Authorization", "RefreshToken")
